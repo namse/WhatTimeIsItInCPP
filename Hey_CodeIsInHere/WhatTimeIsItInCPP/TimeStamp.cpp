a7ce44a9ee8dad3fe036f7b2409cf629dbaea344
Echo <skatpgusskat@naver.com>
2014-11-16 05:24:17 +0900

    performance improvement

--- Hey_CodeIsInHere/WhatTimeIsItInCPP/TimeStamp.cpp
+++ Hey_CodeIsInHere/WhatTimeIsItInCPP/TimeStamp.cpp
@@ -2,28 +2,24 @@
 
 TimeStamp* GTimeStamp = nullptr;
 
-TimeStamp::TimeStamp()
-{
-}
-
-
-TimeStamp::~TimeStamp()
-{
-}
 
 void TimeStamp::StartTimer()
 {
 	auto now = std::chrono::system_clock::now();
-	m_StandardTime = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch());
+	m_StandardTime = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch())
+		+ std::chrono::hours(m_UTC);
 	
 	QueryPerformanceFrequency(&m_Frequency);
 	QueryPerformanceCounter(&m_StandardPerformanceCount);
+
+	SetDefaultDay();
 }
 
 Time TimeStamp::WhatTimeIsIt()
 { 
 	LARGE_INTEGER currentPerformanceCount, elapsedMicroseconds;
 	QueryPerformanceCounter(&currentPerformanceCount);
+
 
 	elapsedMicroseconds.QuadPart = currentPerformanceCount.QuadPart - m_StandardPerformanceCount.QuadPart;
 	
@@ -42,16 +38,75 @@ Time TimeStamp::WhatTimeIsIt()
 	ret.micro = currentTime % 1000;
 	ret.milli = (currentTime / 1000) % 1000;
 	
-	std::time_t t = (time_t)(currentTime / 1000000);
-	std::tm * tm = std::localtime(&t);
-	
-	// Make Sure.. It is 21' Century!
-	ret.year = tm->tm_year - 100;
-	ret.month = tm->tm_mon + 1;
-	ret.day = tm->tm_mday + 1;
-	ret.hour = tm->tm_hour;
-	ret.minute = tm->tm_min;
-	ret.second = tm->tm_sec;
 
+
+	std::time_t t = (time_t)(currentTime / 1000000);
+
+	ret.second = t % 60;
+	ret.minute = (t / 60) % 60;
+	ret.hour = (t / 3600) % 24 + m_UTC;
+	if (m_StandardEpochDay != t / (3600 * 24))
+		UpdateDefaultDay(t / (3600 * 24));
+	ret.day = m_StandardDay;
+	ret.month = m_StandardMonth;
+	ret.year = m_StandardYear;
+
+	LARGE_INTEGER test, testElapsed;
+	QueryPerformanceCounter(&test);
+	testElapsed.QuadPart = test.QuadPart - currentPerformanceCount.QuadPart;
+	testElapsed.QuadPart *= 1000000;
+	testElapsed.QuadPart /= m_Frequency.QuadPart;
 	return ret;
+}
+#include <iostream>
+void TimeStamp::SetDefaultDay()
+{
+	std::time_t t = (time_t)(m_StandardTime.count() / 1000000);
+	std::tm* tm = std::gmtime(&t);
+	
+	
+	m_StandardYear = tm->tm_year + 1900;
+	m_StandardMonth = tm->tm_mon + 1;
+	m_StandardDay = tm->tm_mday;
+	m_StandardEpochDay = ((t / 3600) + m_UTC) / 24;
+	m_StandardGDay = GetGDay(tm->tm_year, tm->tm_mon + 1, tm->tm_mday) - m_StandardEpochDay;
+}
+
+void TimeStamp::UpdateDefaultDay(int dayFromEpoch)
+{
+	GetDate(m_StandardGDay + dayFromEpoch,
+		m_StandardYear,
+		m_StandardMonth,
+		m_StandardDay);
+	if (IS_21_CENTURY)
+		m_StandardYear -= 100;
+
+	m_StandardEpochDay = dayFromEpoch;
+}
+
+int TimeStamp::GetGDay(int year, int month, int day)
+{
+	int y = year;
+	int m = month;
+	int d = day;
+	m = (m + 9) % 12;
+	y = y - m / 10;
+	return 365 * y + y / 4 - y / 100 + y / 400 + (m * 306 + 5) / 10 + (d - 1);
+}
+
+void TimeStamp::GetDate(IN int GDay, OUT int& year, OUT int& month, OUT int& day)
+{
+	int y = (10000 * GDay + 14780) / 3652425;
+	int ddd = GDay - (365 * y + y / 4 - y / 100 + y / 400);
+	if (ddd < 0)
+	{
+		y = y - 1;
+		ddd = GDay - (365 * y + y / 4 - y / 100 + y / 400);
+	}
+	int mi = (100 * ddd + 52) / 3060;
+
+	// return
+	month = (mi + 2) % 12 + 1;
+	year = y + (mi + 2) / 12 + 1900;
+	day = ddd - (mi * 306 + 5) / 10 + 1;
 }
--- Hey_CodeIsInHere/WhatTimeIsItInCPP/TimeStamp.h
+++ Hey_CodeIsInHere/WhatTimeIsItInCPP/TimeStamp.h
@@ -4,23 +4,43 @@
 #include <ctime>
 #include <time.h>
 #include <Windows.h>
+
+#define IS_21_CENTURY true
+
 class TimeStamp
 {
 public:
-	TimeStamp();
-	~TimeStamp();
+	TimeStamp(int UTC = +9) : m_UTC(UTC)
+	{};
+	~TimeStamp() 
+	{};
 
-	void StartTimer();
+	// default setting is Seoul.
+	void StartTimer(); 
 	// because windows use function name - "GetCurrentTime", 
 	// i name this function as joke. plz rename on your project as you want.
 	Time WhatTimeIsIt(); 
 
 private:
+	// We Should Calculate Y/M/D First.
+	// Because that calculation is too heavy.
+	void SetDefaultDay();
+	void UpdateDefaultDay(int dayFromEpoch);
+
+	// Algorithm from http://alcor.concordia.ca/~gpkatch/gdate-algorithm.html
+	int GetGDay(int year, int month, int day);
+	void GetDate(IN int GDay, OUT int& year, OUT int& month, OUT int& day);
+private:
 	std::chrono::microseconds m_StandardTime;
 	LARGE_INTEGER m_StandardPerformanceCount;
 	LARGE_INTEGER m_Frequency;
 	
-	
+	int m_UTC;
+	int m_StandardYear;
+	int m_StandardMonth;
+	int m_StandardDay;
+	int m_StandardEpochDay;
+	int m_StandardGDay;
 };
 
 
